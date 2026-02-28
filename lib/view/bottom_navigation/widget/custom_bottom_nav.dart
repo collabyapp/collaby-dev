f8a5a1c580103ac46d47c49b53382af14f60c240
Damian <collabyco@gmail.com>
2026-02-28 15:30:05 +0100

    ui(bottom-nav): lift nav items upward for better vertical alignment on mobile

--- lib/view/bottom_navigation/widget/custom_bottom_nav.dart
+++ lib/view/bottom_navigation/widget/custom_bottom_nav.dart
@@ -27,27 +27,31 @@ class CustomBottomNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      return Container(
-        height: 83,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(24),
-            topRight: Radius.circular(24),
-          ),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10,
-              offset: Offset(0, -2),
+      return SafeArea(
+        top: false,
+        minimum: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: Container(
+          height: 83,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
             ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: List.generate(
-            icons.length,
-            (index) => _buildNavItem(index, icons[index], labels[index]),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                offset: Offset(0, -2),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(
+              icons.length,
+              (index) => _buildNavItem(index, icons[index], labels[index]),
+            ),
           ),
         ),
       );
@@ -59,39 +63,42 @@ class CustomBottomNav extends StatelessWidget {
 
     return InkWell(
       onTap: () => controller.changeTabIndex(index),
-      child: Container(
-        width: 90,
-        height: 47,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          color: isSelected ? Color(0xff4C1CAE) : Colors.transparent,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              '${assetName}',
-              width: 22,
-              height: 22,
-              color: (isSelected
-                  ? AppColor.whiteColor
-                  : const Color(0xff0A0A0A)),
-            ),
-            const SizedBox(width: 4),
-            Text(
-              label.tr,
-              textAlign: TextAlign.center,
-              style: AppTextStyles.extraSmallText.copyWith(
-                fontSize: 10,
-                fontFamily: isSelected
-                    ? AppFonts.OpenSansBold
-                    : AppFonts.OpenSansMedium,
-                color: isSelected
+      child: Transform.translate(
+        offset: const Offset(0, -5),
+        child: Container(
+          width: 90,
+          height: 47,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: isSelected ? Color(0xff4C1CAE) : Colors.transparent,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                '${assetName}',
+                width: 22,
+                height: 22,
+                color: (isSelected
                     ? AppColor.whiteColor
-                    : const Color(0xff979797),
+                    : const Color(0xff0A0A0A)),
               ),
-            ),
-          ],
+              const SizedBox(width: 4),
+              Text(
+                label.tr,
+                textAlign: TextAlign.center,
+                style: AppTextStyles.extraSmallText.copyWith(
+                  fontSize: 10,
+                  fontFamily: isSelected
+                      ? AppFonts.OpenSansBold
+                      : AppFonts.OpenSansMedium,
+                  color: isSelected
+                      ? AppColor.whiteColor
+                      : const Color(0xff979797),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
